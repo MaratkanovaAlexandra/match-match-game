02cb3517b93b739f7daea41374557d1bd2bd282f
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-05-07 16:44:10 +0400

    init: match-match-game

---
+++ src/components/header/header.ts
@@ -0,0 +1,8 @@
+import "./header.scss";
+export class Header {
+    header: HTMLElement;
+    constructor() {
+        this.header = document.createElement("header");
+        this.header.classList.add("header")
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import "./style.scss";
+import "./components/header/header.ts"
+import { Header } from "./components/header/header";
+const ROOT = document.querySelector(".root");
+const HEADER = new Header();
+ROOT.appendChild(HEADER.header);
