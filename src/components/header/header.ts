04f7e34bd8aaf9906f1115d8b2977a1d41a92e8d
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-05-08 17:08:36 +0400

    fix: add header wrapper

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -10,11 +10,13 @@ export class Header {
     this.header = document.createElement("header");
     this.header.classList.add("header");
 
-    const LOGO = createAndAppendHtmlElement(this.header,"div","header__logo");
+    const HEADER_TEMS = createAndAppendHtmlElement(this.header,"div","header__wrapper");
+
+    const LOGO = createAndAppendHtmlElement(HEADER_TEMS, "div","header__logo");
     createAndAppendHtmlElement(LOGO,"div","header__logo-item1", "match");
     createAndAppendHtmlElement(LOGO,"div","header__logo-item2", "match");
 
-    const NAV = createAndAppendHtmlElement(this.header, "nav", "header__nav");
+    const NAV = createAndAppendHtmlElement(HEADER_TEMS, "nav", "header__nav");
     this.about_game = createAndAppendHtmlElement(NAV, "div", "header__nav_item");
     createAndAppendHtmlElement(this.about_game, "div", "header__nav_item-about-game");
     createAndAppendHtmlElement(this.about_game, "div", "header__nav_item-text", "About Game");
@@ -28,6 +30,6 @@ export class Header {
     createAndAppendHtmlElement(this.about_game, "div", "header__nav_item-settings");
     createAndAppendHtmlElement(this.about_game, "div", "header__nav_item-text", "Game Settings");
 
-    this.register_button =createAndAppendHtmlElement(this.header, "button", "header__button", "register new player");
+    this.register_button =createAndAppendHtmlElement(HEADER_TEMS, "button", "header__button", "register new player");
   }
 }
