e8aa5083461608b95046a9a0db75953368ef7906
Andrey Chernobrovkin <adam.holo@mail.ru>
2021-05-08 12:27:02 +0400

    fix: images are filled with the hash not the image name

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
           {
             loader: 'file-loader',
             options:{
-              name: `./img/${`[name].[ext]`}`,
+              name: `./img/${`[contenthash].[ext]`}`,
             },
           }
         ],
