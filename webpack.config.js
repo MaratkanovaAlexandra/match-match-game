cd851b976159746326339c8b540da952150261b3
Andrey Chernobrovkin <adam.holo@mail.ru>
2021-05-11 18:27:09 +0400

    add: plugin (HTMLWebpackPlugin , CleanWebpackPlugin, devServer) and changing the path to index.html up to the js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,30 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const plugins = () => {
+  const basePlugins = [
+      new HTMLWebpackPlugin({
+          template: path.resolve(__dirname, './index.html'),
+          filename: 'index.html',
+          inject: true
+      }),
+      new CleanWebpackPlugin(),
+  ];
+  return basePlugins;
+}
+
 
 module.exports = {
   entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: plugins(),
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   module: {
     rules: [
       {
@@ -23,15 +46,15 @@ module.exports = {
             },
           }
         ],
-      }
+      },   
     ],
   },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  devServer:{
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, 'dist'),
+    open: true,
+    hot: true,
+    compress: true,
+    port: 3000
   },
 };
