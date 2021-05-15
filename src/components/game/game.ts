34aaf34d6b9e459a1f0f42346541501ec15234d5
Andrey(laptop) <adam.holo@mail.ru>
2021-05-15 14:49:24 +0400

    add: shuffle of cards added to the game class

--- src/components/game/game.ts
+++ src/components/game/game.ts
@@ -5,13 +5,14 @@ const createAndAppendHtmlElement = require( "../../add-element-function");
 
 export class Game {
     game: HTMLElement;
-    private _time: 0;
-    private _score: 0;
-    private _moves: 0;
-    private _wrong_moves: 0;
-    private _you_win: false;
+    private _time:number = 0;
+    private _score:number =  0;
+    private _moves:number  = 0;
+    private _wrong_moves:number = 0;
+    private _you_win:boolean = false;
     private _width_game: number;
     private _height_game: number;
+    private _arr_cards: Card[] = [];
     private _pairCards: {
         card1: null,
         card2: null
@@ -25,7 +26,6 @@ export class Game {
         if (this._difficulty % 2 !== 0) this._height_game = this._difficulty + 1;
         else this._height_game = this._difficulty;
         
-
         this.game = document.createElement("div");
         this.game.classList.add("game");
 
@@ -34,22 +34,52 @@ export class Game {
         const timer_wrapper = createAndAppendHtmlElement(timer, "div", "timer__wrapper");
         timer_wrapper.innerText = "00:00";
 
+        for(let j = 0; j < this._height_game * (this._width_game / 2); j++) {
+            this.createPairCards();
+        }
+
+        this._arr_cards = this.mixArrCards(this._arr_cards, 5);
+
         const game_wrapper = createAndAppendHtmlElement(this.game, "div", "game__wrapper");
-        for(let i = 0; i < this._height_game; i++) {
-            const game_row = createAndAppendHtmlElement(game_wrapper, "div", "game__row");
-            for(let j = 0; j < this._width_game / 2; j++) {
-                this.createPairCards(game_row);
+        this.drawCards(game_wrapper);
+    }
+
+    mixArrCards(array:Card[], countMix:number){
+        for(let c = 0; c < countMix; c++){
+            for (let i = (array.length - 1); i > 0; i--) {
+                let j = Math.floor(Math.random() * (i + 1));
+                console.log(j);
+                const temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
             }
         }
+        return array;
     }
+
     getRandomArbitrary(min:number, max:number) {
         return Math.floor(Math.random() * (max - min) + min);
     }
-    createPairCards(row:HTMLElement) {
+
+    createPairCards() {
         const random_img_number = this.getRandomArbitrary(1, 51);
-        const card1 = new Card(random_img_number, random_img_number).card;
-        const card2 = new Card(random_img_number, random_img_number).card;
-        row.appendChild(card1);
-        row.appendChild(card2);
+        const card1 = new Card(random_img_number, random_img_number);
+        const card2 = new Card(random_img_number, random_img_number);
+
+        this._arr_cards.push(card1);
+        this._arr_cards.push(card2);
+
+        //row.appendChild(card1.card);
+        //row.appendChild(card2.card);
+    }
+
+    drawCards(parent:HTMLElement) {
+        let game_row = createAndAppendHtmlElement(parent, "div", "game__row");
+        for (let i = 0; i < this._arr_cards.length; i++) {
+            game_row.appendChild(this._arr_cards[i].card)
+            if ( (i + 1) % this._width_game === 0 ) {
+                game_row = createAndAppendHtmlElement(parent, "div", "game__row");
+            }  
+        }
     }
 }
