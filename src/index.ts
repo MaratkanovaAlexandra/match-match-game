2594e1888becd73f236807ddf86d2f0dce0abb52
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-05-28 15:11:04 +0400

    fix: remove .root from index.html

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import "./styles/main.scss";
 import { Header } from "./components/header/header";
 import { HowToPlay } from "./components/how_to_play/how_to_play";
 
-const ROOT = document.querySelector(".root");
+
+const ROOT = document.createElement("div");
+ROOT.classList.add(".root");
+document.body.appendChild(ROOT);
+
 const HEADER = new Header();
 HEADER.init();
 const HOWTOPlAY = new HowToPlay();
